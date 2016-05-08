043013bf4e90bc8d91c0d758c9dc07ac5f5b0423
Benjamin Koffel <benjamink@beteasy.com.au>
2016-05-08 19:29:23 +1000

    Add notification listener client.

--- bring-up.sh
+++ bring-up.sh
@@ -1,4 +1,4 @@
 docker run -d --name redis-server -p 6379:6379 redis
-docker run -d --name rabbitmq-server --hostname rabbitmq-server -e RABBITMQ_ERLANG_COOKIE='aleoEkdiwDFIEnjEE39322DECcsWPe' -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+docker run -d --name rabbitmq-server --hostname rabbitmq-server -e RABBITMQ_ERLANG_COOKIE='aleoEkdiwDFIEnjEE39322DECcsWPe' -e RABBITMQ_DEFAULT_USER=user -e RABBITMQ_DEFAULT_PASS=password -p 15672:15672 -p 5672:5672 -p 15674:15674 benji:rabbitMq
 docker run -d -e PYTHONUNBUFFERED=0 benji:demoWorker
 docker run -d -p 80:80 benji:demoApi
