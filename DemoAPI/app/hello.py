5398893858000c6a0b1419345d3b4028801340d0
Benjamin Koffel <benjamink@beteasy.com.au>
2016-05-09 22:41:10 +1000

    Fix APIs and add a html client to test demo that it works.

--- DemoAPI/app/hello.py
+++ DemoAPI/app/hello.py
@@ -9,9 +9,25 @@ app = Flask(__name__)
 
 redisConn = redis.Redis('192.168.99.100', 6379)
 
+@app.route('/')
+def index():		
+	list = []	
+	for key in redisConn.scan_iter():
+		value = redisConn.get(key)
+		data = {}
+		data['key'] = str(key, 'utf-8')
+		data['value'] = str(value, 'utf-8')
+		list.append(data)		
+	print(list)
+	return json.dumps(list)
+
 @app.route('/<key>')
 def get(key):	
-	return redisConn.get(key)
+	value = redisConn.get(key)
+	data = {}
+	data['key'] = key
+	data['value'] = str(value, 'utf-8')
+	return json.dumps(data)
 	
 @app.route('/<key>/<value>')
 def save(key, value):	
@@ -22,7 +38,25 @@ def save(key, value):
 	data['key'] = key
 	data['value'] = value
 	rabbitChannel.basic_publish(exchange='', routing_key='command.saveKeyValue', body=json.dumps(data)	, properties=pika.BasicProperties(delivery_mode=2))
-	return "value submitted"
+	return json.dumps(data)
+	
+#@app.route('/', methods=['POST'])
+#def post():
+	#rabbitConn = pika.BlockingConnection(pika.ConnectionParameters('192.168.99.100', 5672, '/', credentials=pika.PlainCredentials('user', 'password')))
+	#rabbitChannel = rabbitConn.channel()
+	#rabbitChannel.queue_declare(queue='command.saveKeyValue', durable=True)
+	#data = {}
+	#data['key'] = request.form['key'];
+	#data['value'] = request.form['value'];
+	#rabbitChannel.basic_publish(exchange='', routing_key='command.saveKeyValue', body=json.dumps(data)	, properties=pika.BasicProperties(delivery_mode=2))
+	#return json.dumps(data)
 
+@app.after_request
+def after_request(response):
+	response.headers.add('Access-Control-Allow-Origin', '*')
+	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+	response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+	return response
+  
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ build-images.sh
@@ -0,0 +1,3 @@
+docker build -t benji:demoApi DemoAPI/
+docker build -t benji:demoWorker DemoWorker/
+docker build -t benji:rabbitMq RabbitMQ/
