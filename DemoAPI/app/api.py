65fe5f8da168ff6643b795ad42eb91233c39ec73
Benjamin Koffel <benjamink@beteasy.com.au>
2016-05-29 22:05:28 +1000

    Update some file.s

--- DemoAPI/app/api.py
+++ DemoAPI/app/api.py
@@ -1,6 +1,5 @@
-#!flask/bin/python
-from flask import render_template
-from flask import Flask
+#!/usr/bin/env python
+from flask import Flask, abort, make_response, request
 import redis
 import pika
 import json
@@ -9,37 +8,67 @@ app = Flask(__name__)
 
 redisConn = redis.Redis('192.168.99.100', 6379)
 
+rabbitConn = pika.BlockingConnection(pika.ConnectionParameters('192.168.99.100', 5672, '/', credentials=pika.PlainCredentials('user', 'password')))
+
+rabbitChannel = rabbitConn.channel()
+
+rabbitChannel.queue_declare(queue='command.saveKeyValue', durable=True)
+
 @app.route('/')
-def index():		
-	list = []	
+def index():
+
+	dataList = []
+
 	for key in redisConn.scan_iter():
+
 		value = redisConn.get(key)
-		data = {}
-		data['key'] = str(key, 'utf-8')
-		data['value'] = str(value, 'utf-8')
-		list.append(data)		
-	print(list)
-	return json.dumps(list)
+
+		if value is not None:
+
+			data = {}
+			data['key'] = str(key, 'utf-8')
+			data['value'] = str(value, 'utf-8')
+
+		dataList.append(data)
+
+	return make_response(json.dumps(dataList), 200)
 
 @app.route('/<key>')
-def get(key):	
+def get(key):
+
 	value = redisConn.get(key)
+
+	if value is None:
+
+		abort(404)
+
 	data = {}
 	data['key'] = key
 	data['value'] = str(value, 'utf-8')
-	return json.dumps(data)
-	
+
+	return make_response(json.dumps(data), 200)
+
 @app.route('/<key>/<value>')
-def save(key, value):	
-	rabbitConn = pika.BlockingConnection(pika.ConnectionParameters('192.168.99.100', 5672, '/', credentials=pika.PlainCredentials('user', 'password')))
-	rabbitChannel = rabbitConn.channel()
-	rabbitChannel.queue_declare(queue='command.saveKeyValue', durable=True)
+def save(key, value):
+
 	data = {}
 	data['key'] = key
 	data['value'] = value
-	rabbitChannel.basic_publish(exchange='', routing_key='command.saveKeyValue', body=json.dumps(data)	, properties=pika.BasicProperties(delivery_mode=2))
-	return json.dumps(data)
-	
+
+	rabbitChannel.basic_publish(exchange='', routing_key='command.saveKeyValue', body=json.dumps(data), properties=pika.BasicProperties(delivery_mode=2))
+
+	return make_response(json.dumps(data), 201)
+
+@app.route('/', methods=['POST'])
+def post():
+
+	data = request.json
+
+	rabbitChannel.basic_publish(exchange='', routing_key='command.saveKeyValue', body=json.dumps(data), properties=pika.BasicProperties(delivery_mode=2))
+
+	return make_response(json.dumps(data), 201)
+
+
 #@app.route('/', methods=['POST'])
 #def post():
 	#rabbitConn = pika.BlockingConnection(pika.ConnectionParameters('192.168.99.100', 5672, '/', credentials=pika.PlainCredentials('user', 'password')))
@@ -53,10 +82,13 @@ def save(key, value):
 
 @app.after_request
 def after_request(response):
+
 	response.headers.add('Access-Control-Allow-Origin', '*')
 	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
 	response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+
 	return response
-  
+
 if __name__ == '__main__':
+
     app.run(debug=True)
--- DemoAPI/app/test.py
+++ DemoAPI/app/test.py
@@ -1,15 +1,59 @@
 import unittest
 
-from hello import app
+import json
+import redis
+from api import app
+
+class TestDemoApi(unittest.TestCase):
 
 
-class TestPost(unittest.TestCase):
+    def setUp(self):
+
+        redisConn = redis.Redis('192.168.99.100', 6379)
+
+        redisConn.set('ebddbb7d-29f3-4cb8-877f-313d1d6e296f', 'test value')
+
+    def test_index(self):
+
+        self.test_app = app.test_client()
+
+        response = self.test_app.get('/', content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertIsNotNone(response.data)
+
+    def test_get_notfound(self):
+
+        self.test_app = app.test_client()
+
+        response = self.test_app.get('/12b07c79-92e1-4e6b-a4b7-49dd2bdba9a3', content_type='application/json')
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_get_found(self):
+
+        self.test_app = app.test_client()
+
+        response = self.test_app.get('/ebddbb7d-29f3-4cb8-877f-313d1d6e296f', content_type='application/json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.data)
+        #self.assertEqual('ebddbb7d-29f3-4cb8-877f-313d1d6e296f', response.data['key'])
+        #self.assertEqual('test value', response.data['value'])
+
     def test_post(self):
 
         self.test_app = app.test_client()
 
-        response = self.test_app.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
+        data = {}
+        data['key'] = ''
+        data['value'] = 'value'
+
+        response = self.test_app.post('/', content_type='application/json', data=json.dumps(data))
+
+        self.assertEqual(response.status_code, 201)
 
 if __name__ == '__main__':
+
     unittest.main()
